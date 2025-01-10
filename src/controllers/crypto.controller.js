5985f5a254e1be76157ef1f45edfecc67c8f0dc5
Tushar Jain <tusharjain01234@gmail.com>
2025-01-10 23:36:27 +0530

    feat(crypto.controller.js): add getCryptoDeviation function to calculate price standard deviation

--- src/controllers/crypto.controller.js
+++ src/controllers/crypto.controller.js
@@ -1,6 +1,7 @@
 import Crypto from '../models/Crypto.js';
-import { ApiResponse } from '../utils/ApiError.util.js';
 import { ApiError } from '../utils/ApiError.util.js';
+import { ApiResponse } from '../utils/ApiError.util.js';
+import { standardDeviation } from '../utils/helpers.js';
 
 export const getCryptoStats = async (req, res, next) => {
   try {
@@ -18,7 +19,30 @@ export const getCryptoStats = async (req, res, next) => {
       price: latestRecord.price,
       marketCap: latestRecord.marketCap,
       '24hChange': latestRecord.change24h,
-    });
+    }, 'Crypto stats fetched successfully');
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const getCryptoDeviation = async (req, res, next) => {
+  try {
+    const { coin } = req.query;
+    if (!coin) {
+      throw new ApiError(400, 'Coin is required');
+    }
+
+    const records = await Crypto.find({ coin }).sort({ fetchedAt: -1 }).limit(100);
+    if (records.length < 2) {
+      throw new ApiError(400, 'Not enough data to calculate deviation');
+    }
+
+    const prices = records.map(record => record.price);
+    const deviation = standardDeviation(prices);
+
+    const response = new ApiResponse(200, { deviation });
     res.status(200).json(response);
   } catch (error) {
     next(error);
